f8bf5995d7688770efff25c1125f6aa3ffb6ba71
DBertazioli <d.bertazioli@campus.unimib.it>
2023-03-29 18:05:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,22 +9,22 @@ nvidia-xconfig -a --virtual=$RESOLUTION --allow-empty-initial-configuration --en
 Xorg :0 &
 sleep 1  # wait for the server gets ready
 
-# 2. start x11 and vnc connection
-# to inspect logs in detail, use --verbose
-x11vnc -display :0 -passwd $VNCPASS -forever -rfbport 5900 &
-sleep 2  # wait for the server gets ready
+# # 2. start x11 and vnc connection
+# # to inspect logs in detail, use --verbose
+# x11vnc -display :0 -passwd $VNCPASS -forever -rfbport 5900 &
+# sleep 2  # wait for the server gets ready
 
 # 2.5 start audio
 # this is not required.
 # pulseaudio --start
 # sleep 2
 
-# 3. start noVNC
-/noVNC-1.1.0/utils/launch.sh --vnc localhost:5900 --listen 8081 &
-sleep 2
+# # 3. start noVNC
+# /noVNC-1.1.0/utils/launch.sh --vnc localhost:5900 --listen 8081 &
+# sleep 2
 
-echo 'running noVNC at http://localhost:8081/vnc.html?host=localhost&port=8081'
+# echo 'running noVNC at http://localhost:8081/vnc.html?host=localhost&port=8081'
 
 # 3. start simulator
 export DISPLAY=:0
-openbox
+# openbox
